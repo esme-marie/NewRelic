76271037a4a874e8e61af29bb458d73cebb76992
Esme-Marie <esmemarie.sultan@gmail.com>
2021-02-20 20:19:33 +0800

    Updates

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 }
-app.use(routes);
+// app.use(routes);
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
