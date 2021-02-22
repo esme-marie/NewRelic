1e24924ac53aa004555886cb988cb8f14094ebca
jas4gan <jas4gan@gmail.com>
2021-02-23 01:00:15 +0800

    update donate links and added back button to homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Routes from "./routes";
-import LeafMap from './components/Map/MyMap'
 const App = () => {
   return (
     <div>
-      <Routes />
-      
+      <Routes />      
     </div>
   )
 }
