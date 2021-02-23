a6e8b237397b8f4ce9497d6d4570d0f190e1dbd4
belle1306 <belle1306@gmail.com>
2021-02-23 14:29:39 +0800

    added insta links

--- client/src/components/Three.js
+++ client/src/components/Three.js
@@ -181,7 +181,8 @@ export default class ThreeHome extends Component {
               <header>
                 <a href="/Globalforestwatch" className="top-left" children="Explore real time data with Global Forest Watch" />
                 <a href='/Quiz' className="top-right" children="Quiz" />
-                <a href="/message" className="bottom-right" children="Message Aliens" />
+                {/* <a href="/message" className="bottom-right" children="Message Aliens" /> */}
+                <a href="/instaLinks" className="bottom-right" children="Follow Sustainable Insta" />
                 <a href="/apod" className="bottom-left" children="Astronomical Birthday" />
                 <a href="/map" className="bottom-center" children="Leaflet Map" />
               </header>
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import { Route, Switch} from 'react-router-dom'; //deleted withRouter, BrowserRouter,
-import MessageForm from './components/messageForm';
+// import MessageForm from './components/messageForm';
 import ThreeHome from './components/Three';
 import APOD from './components/apod';
-import solarscope from './components/solarscope';
 import QuizForm from './components/quiz';
-import MessageQuotes from './components/messageQuotes';
+// import MessageQuotes from './components/messageQuotes';
 import GetLeaderBoard from './components/getleaderboard';
 import Globalforestwatch from './components/Globalforestwatch';
-import LeafMap from './components/Map/MyMap'
+import LeafMap from './components/Map/MyMap';
+import InstaLinks from './components/instaLinks';
 
 
 
@@ -19,11 +19,12 @@ class Routes extends Component {
             <Switch>
                 <Route path="/apod" component={APOD} />
                 <Route path="/map" component={LeafMap} />
-                <Route path="/message" component={MessageForm} />
+                {/* <Route path="/message" component={MessageForm} /> */}
+                <Route path="/instaLinks" component={InstaLinks} />
                 <Route path="/Globalforestwatch" component={Globalforestwatch} />
                 {/* <Route path="/solarscope" component={solarscope} /> */}
                 <Route path="/quiz" component={QuizForm} />
-                <Route path="/messageQuotes" component={MessageQuotes} />
+                {/* <Route path="/messageQuotes" component={MessageQuotes} /> */}
                 {/* <Route path="/" component={Home} /> */}
                 <Route path="/getleaderboard" component={GetLeaderBoard}/>
                 <Route path="/" component={ThreeHome} />
