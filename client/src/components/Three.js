cd27a5fd278a5c44dd4da9aaa614ee5a7e510423
Khalilah <dygkhalilah@gmail.com>
2021-02-25 20:24:26 +0800

    REL-21 displayed animals' name on globe and fixed coordinates

--- client/src/components/Three.js
+++ client/src/components/Three.js
@@ -1,221 +1,3 @@
-// import * as THREE from 'three';
-// import React, { Component } from 'react';
-// import '../App.css';
-
-// export default class ThreeHome extends Component {
-//   componentDidMount() {
-//       let scene = new THREE.Scene()
-//       // scene.fog = new THREE.FogExp2(0x000000, 0.00025)
-//       let camera = new THREE.PerspectiveCamera(
-//           35, 
-//           window.innerWidth / window.innerHeight, 
-//           0.1, 
-//           4000
-//           );
-//           camera.position.z = 25 //-1000
-      
-//       let renderer = new THREE.WebGLRenderer({
-//           antialias: true
-//       });
-//       renderer.setSize(window.innerWidth, window.innerHeight)
-//       renderer.setPixelRatio(window.devicePixelRatio)
-//       renderer.setClearColor(0x000000, 1)
-      
-//       // //   MOUNT INSIDE A REACT
-//       this.mount.appendChild(renderer.domElement)
-      
-//       // add some lighting
-//       const ambientLight = new THREE.AmbientLight(0x777777)
-//       scene.add(ambientLight)
-      
-//       // add a spotlight
-//       const pointLight = new THREE.PointLight(0xffffff, 1, 0)
-//       pointLight.position.set(500, 500, -2000)
-//       scene.add(pointLight)
-      
-//       // make a THREE.js loader
-//       const loader = new THREE.TextureLoader()
-//       loader.load(
-//           "eae.png",
-//           this.onLoad,
-//           this.onProgress,
-//           this.onError
-//       );
-
-//       // make planet 
-//       const makePlanet = function () {
-//       const texture = loader.load("eae.png");
-//       const geometry = new THREE.SphereGeometry(600, 128, 128)
-//       const material = new THREE.MeshLambertMaterial({
-//           map: texture
-//       })
-//       const mesh = new THREE.Mesh(geometry, material)
-//       scene.add(mesh)
-//       return mesh
-//       }
-
-//       // make stars
-//       const makeStars = function (url, maxNum) {
-//       const texture = loader.load(url)
-//       const geometry = new THREE.Geometry()
-      
-//       for (let i = 0; i < maxNum; i = i + 1) {
-//           const point = new THREE.Vector3()
-//           const sphericalPoint = new THREE.Spherical(
-//               900 + Math.random() * 900,
-//           2 * Math.PI * Math.random(),
-//           Math.PI * Math.random()
-//           )
-          
-//           point.setFromSpherical(sphericalPoint)
-          
-//           geometry.vertices.push(point)
-//       }
-      
-//       const material = new THREE.PointsMaterial({
-//           size: 50,
-//           map: texture,
-//           transparent: true,
-//           blending: THREE.AdditiveBlending,
-//           depthTest: true,
-//           depthWrite: false
-//       })
-      
-//       const points = new THREE.Points(geometry, material)
-      
-//       scene.add(points)
-      
-//       return points
-//       }
-      
-//       const earth = makePlanet()
-//       // eslint-disable-next-line
-//       const stars2 = makeStars("particle.png", 1000)
-      
-
-//       // hold the camera positions
-//       let currentX = 0
-//       let currentY = 0
-//       let aimX = 0
-//       let aimY = 0
-
-      
-//         // animate the scene
-//       const animate = function () {
-//           const diffX = aimX - currentX
-//           const diffY = aimY - currentY
-          
-//           currentX = currentX + diffX * 0.01
-//           currentY = currentY + diffY * 0.01
-          
-//           const sphere = new THREE.Spherical(
-//               3000,
-//             (currentY * 0.001) - Math.PI / 2,
-//             (currentX * 0.001)
-//           )
-          
-//           camera.position.setFromSpherical(sphere)
-          
-//           // camera.position.x = currentX
-//           // camera.position.y = currentY  
-          
-//           camera.lookAt(scene.position)
-          
-//           earth.rotateY(0.005)
-//           //   line.rotateY(0.01)
-          
-//           // stars2.geometry.rotateX(0.004)
-
-//           renderer.render(scene, camera)
-          
-//           requestAnimationFrame(animate)
-//       }
-//       animate();
-      
-//       window.addEventListener("resize", function () {
-//       camera.aspect = window.innerWidth / window.innerHeight
-//       camera.updateProjectionMatrix()
-      
-//       renderer.setSize(window.innerWidth, window.innerHeight)
-//       renderer.render( scene, camera );
-//       })
-
-//       let raycaster = new THREE.Raycaster();
-//       let mouse = new THREE.Vector2();
-      
-      
-//       let isMouseDown = false
-//       let startX = 0
-//       let startY = 0
-      
-//       document.addEventListener("mousedown", function (event) {
-//       isMouseDown = true
-//       startX = event.pageX
-//       startY = event.pageY
-//       })
-      
-//       document.addEventListener("mouseup", function () {
-//       isMouseDown = false
-//       })
-      
-//       document.addEventListener("mousemove", function (event) {
-//       if (isMouseDown) {
-//           // aimX = ((window.innerWidth / 2) - event.pageX) * 4
-//           // aimY = ((window.innerHeight / 2) - event.pageY) * 4
-//           aimX = aimX + ((event.pageX - startX) * 8)
-//           aimY = aimY + ((event.pageY - startY) * 8)
-//           startX = event.pageX
-//           startY = event.pageY
-//       }
-//       })
-      
-//       document.addEventListener("touchmove", function (event) {
-//       aimX = ((window.innerWidth / 2) - event.pageX) * 4
-//       aimY = ((window.innerHeight / 2) - event.pageY) * 4
-//       });
-
-//       // document.body.addEventListener('click', () => {
-//       //   animate();
-//       // })
-//       function onMouseMove( event ) {
-
-//         // calculate mouse position in normalized device coordinates
-//         // (-1 to +1) for both components
-      
-//         mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-//         mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-      
-//       // }
-      
-//       // function render() {
-      
-//         // update the picking ray with the camera and mouse position
-//         raycaster.setFromCamera( mouse, camera );
-      
-//         // calculate objects intersecting the picking ray
-//         const intersects = raycaster.intersectObjects( scene.children );
-      
-//         for(let i = 0; i < intersects.length; i ++ ) {
-//           intersects[i].object.material.color.set( 0xff0000 );
-//           window.location.href = '/map';
-//         }
-//         renderer.render( scene, camera );
-//       }
-
-//       // function onDocumentMouseDown(event) {
-       
-//       // }
-//       window.addEventListener( 'click', onMouseMove, false )
-//       // earth.addEventListener('mousedown', onDocumentMouseDown, false);
-//       // window.addEventListener( 'click', onMouseMove, true);
-      
-//       // window.requestAnimationFrame(render);
-//       // document.body.addEventListener('click', function(event){
-
-//       // })
-
-
-// }
   
 //   render() {
 //       return (
@@ -252,7 +34,8 @@ import markers from "./markers";
 import markerRenderer from "./markerRenderer";
 
 const options = {
-  markerRenderer
+  markerRenderer,
+  markerTooltipRenderer: marker => `${marker.city}`
 };
 
 function ThreeHome() {
@@ -266,6 +49,17 @@ function ThreeHome() {
         options={options}
         onMouseOverMarker={(marker, markerObject, event) => console.log(marker, markerObject, event)}
       />
+      <div>
+        <h3>[team name]</h3>
+        <header>
+          <a href="/Globalforestwatch" className="top-left" children="Explore real time data with Global Forest Watch" />
+          <a href='/Quiz' className="top-right" children="Quiz" />
+          {/* <a href="/message" className="bottom-right" children="Message Aliens" /> */}
+          <a href="/instaLinks" className="bottom-right" children="Shop VicTrees" />
+          <a href="/apod" className="bottom-left" children="Astronomical Birthday" />
+          <a href="/map" className="bottom-center" children="Leaflet Map" />
+        </header>
+        </div>
     </div>
   );
 }
