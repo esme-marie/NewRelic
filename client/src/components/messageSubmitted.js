fe0e76e3d849f5cd2a2454f46b40f135e12478ad
belle1306 <belle1306@gmail.com>
2021-02-25 17:26:05 +0800

    REL-1 added form and home button to submit page

--- client/src/components/messageSubmitted.js
+++ client/src/components/messageSubmitted.js
@@ -1,7 +1,7 @@
 // import {Link} from "react-router-dom";
 import React from "react";
 import ThreeStars from '../components/ThreeStars';
-
+import {Link} from "react-router-dom";
 
 
 
@@ -15,13 +15,22 @@ export default class MessageSubmitted extends React.Component {
                 
                 </div>
                     <div id="messageSubmitted">
-                            <h2>⏁⊑⏃⋏☍ ⊬⍜⎍</h2>
-                            <h3>thank you</h3>
+                            <h2></h2>
+                            <h3>Thank you on behalf of the trees</h3>
                             <br/>
                             <p>Your message is on its way to Victrees!</p>
                             <p>In the meantime, do find our other resources at our home page✨</p>
+                            
                             <div className="container-submit">
                             
+                            </div>
+                            <div>
+                            <Link to="/">
+                                <button className="submittedButton" text-align="center"
+                                type="submit">
+                                Home
+                                </button>
+                            </Link>
                             </div>
                     </div>
                     
