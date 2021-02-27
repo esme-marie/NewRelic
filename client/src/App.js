84d7c8666c95e7ca64240063bf2b3fe0a41edb73
LillianToh <56778700+LillianToh@users.noreply.github.com>
2021-02-28 00:30:28 +0800

    final debug

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ThreeHome from "./components/Three";
 import QuizForm from "./components/quiz";
-import Globalforestwatch from "./components/Globalforestwatch";
+import Globalforestwatch from "./components/globalforestwatch";
 import LeafMap from "./components/MyMap";
 import Shop from "./components/shop";
 import MessageForm from "./components/messageForm";
@@ -16,7 +16,7 @@ function App() {
         <Switch>
         <Route path="/takeAction" component={LeafMap} />
         <Route path="/shop" component={Shop} />
-        <Route path="/Globalforestwatch" component={Globalforestwatch} />
+        <Route path="/globalforestwatch" component={Globalforestwatch} />
         <Route path="/quiz" component={QuizForm} />
         <Route path="/message" component={MessageForm} />
         <Route exact path="/" component={ThreeHome} />
